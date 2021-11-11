9dbb682c7893a2f6ecb26ea6297f69e2cca3436f
Rohan Shrestha <rohnsht@gmail.com>
2021-11-11 12:29:11 +1030

    implement different logic

--- lib/flutter_ticket.dart
+++ lib/flutter_ticket.dart
@@ -6,18 +6,23 @@ class TicketCard extends StatefulWidget {
   final EdgeInsets margin;
   final TicketDecoration _decoration;
   final TicketDivider? _divider;
+  final int topFlex, bottomFlex;
   final Widget topChild, bottomChild;
   final GestureTapCallback? onTap;
 
-  TicketCard({Key? key, 
+  TicketCard({
+    Key? key,
     this.margin = const EdgeInsets.all(5),
     TicketDecoration? decoration,
     TicketDivider? divider,
+    this.topFlex = 1,
+    this.bottomFlex = 1,
     required this.topChild,
     required this.bottomChild,
     this.onTap,
   })  : _decoration = decoration ?? TicketDecoration(),
-        _divider = divider, super(key: key);
+        _divider = divider,
+        super(key: key);
 
   @override
   State<StatefulWidget> createState() {
@@ -46,26 +51,40 @@ class _TicketCard extends State<TicketCard> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
+    final clipRatio = widget.topFlex / (widget.topFlex + widget.bottomFlex);
+
     return GestureDetector(
       child: ScaleTransition(
         scale: _animation,
         child: Padding(
           padding: widget.margin,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              CustomPaint(
-                painter: _TopPainter(widget._decoration),
-                child: widget.topChild,
+          child: CustomPaint(
+            painter: _CustomPainter(widget._decoration, clipRatio),
+            child: ClipPath(
+              clipper: _CustomClipper(widget._decoration, clipRatio),
+              child: CustomPaint(
+                foregroundPainter: widget._divider == null
+                    ? null
+                    : _DividerPainter(
+                        widget._divider!,
+                        clipRatio,
+                      ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Flexible(
+                      flex: widget.topFlex,
+                      child: widget.topChild,
+                    ),
+                    Flexible(
+                      flex: widget.bottomFlex,
+                      child: widget.bottomChild,
+                    ),
+                  ],
+                ),
               ),
-              CustomPaint(
-                painter: _BottomPainter(widget._decoration),
-                foregroundPainter: widget._divider != null
-                    ? _DividerPainter(widget._divider!)
-                    : null,
-                child: widget.bottomChild,
-              ),
-            ],
+            ),
           ),
         ),
       ),
@@ -84,86 +103,91 @@ class _TicketCard extends State<TicketCard> with TickerProviderStateMixin {
   }
 }
 
-class _TopPainter extends CustomPainter {
+class _CustomClipper extends CustomClipper<Path> {
   final TicketDecoration decoration;
+  final double clipRatio;
 
-  _TopPainter(this.decoration);
+  _CustomClipper(this.decoration, this.clipRatio);
 
   @override
-  void paint(Canvas canvas, Size size) {
-    var paint = Paint()
-      ..style = PaintingStyle.fill
-      ..color = decoration.topColor;
+  getClip(Size size) {
+    final clipHeight = size.height * clipRatio;
 
-    var path = Path();
-    path.moveTo(0.0, decoration.borderRadius);
-    path.quadraticBezierTo(0, 0, decoration.borderRadius, 0);
-    path.lineTo(size.width - decoration.borderRadius, 0.0);
-    path.quadraticBezierTo(size.width, 0, size.width, decoration.borderRadius);
-    path.lineTo(size.width, size.height - decoration.clipRadius);
-    path.quadraticBezierTo(
-        size.width - decoration.clipRadius,
-        size.height - decoration.clipRadius,
-        size.width - decoration.clipRadius,
-        size.height);
-    path.lineTo(decoration.clipRadius, size.height);
-    path.quadraticBezierTo(
-        decoration.clipRadius,
-        size.height - decoration.clipRadius,
-        0,
-        size.height - decoration.clipRadius);
-    path.close();
+    var path = Path()
+      ..moveTo(0.0, decoration.borderRadius)
+      ..quadraticBezierTo(0, 0, decoration.borderRadius, 0)
+      ..lineTo(size.width - decoration.borderRadius, 0.0)
+      ..quadraticBezierTo(size.width, 0, size.width, decoration.borderRadius)
+      ..lineTo(size.width, clipHeight - decoration.clipRadius)
+      ..arcToPoint(
+        Offset(size.width, clipHeight + decoration.clipRadius),
+        radius: Radius.circular(decoration.clipRadius),
+        clockwise: false,
+      )
+      ..lineTo(size.width, size.height - decoration.borderRadius)
+      ..quadraticBezierTo(size.width, size.height,
+          size.width - decoration.borderRadius, size.height)
+      ..lineTo(decoration.borderRadius, size.height)
+      ..quadraticBezierTo(
+          0, size.height, 0, size.height - decoration.borderRadius)
+      ..lineTo(0, clipHeight + decoration.clipRadius)
+      ..arcToPoint(
+        Offset(0, clipHeight - decoration.clipRadius),
+        radius: Radius.circular(decoration.clipRadius),
+        clockwise: false,
+      )
+      ..close();
 
-    canvas.drawShadow(path, decoration.shadowColor, decoration.elevation, true);
-    canvas.drawPath(path, paint);
+    return path;
   }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+  bool shouldReclip(covariant CustomClipper oldClipper) {
     return false;
   }
 }
 
-class _BottomPainter extends CustomPainter {
+class _CustomPainter extends CustomPainter {
   final TicketDecoration decoration;
+  final double clipRatio;
 
-  _BottomPainter(this.decoration);
+  _CustomPainter(this.decoration, this.clipRatio);
 
   @override
   void paint(Canvas canvas, Size size) {
+    final clipHeight = size.height * clipRatio;
+
     var paint = Paint()
       ..style = PaintingStyle.fill
-      ..color = decoration.bottomColor;
+      ..color = Colors.white;
 
-    var path = Path();
-    path.moveTo(0.0, decoration.clipRadius);
-    path.quadraticBezierTo(
-        decoration.clipRadius, decoration.clipRadius, decoration.clipRadius, 0);
-    path.lineTo(size.width - decoration.clipRadius, 0.0);
-    path.quadraticBezierTo(size.width - decoration.clipRadius,
-        decoration.clipRadius, size.width, decoration.clipRadius);
-    path.lineTo(size.width, size.height - decoration.borderRadius);
-    path.quadraticBezierTo(size.width, size.height,
-        size.width - decoration.borderRadius, size.height);
-    path.lineTo(decoration.borderRadius, size.height);
-    path.quadraticBezierTo(
-        0, size.height, 0, size.height - decoration.borderRadius);
-    path.close();
+    var path = Path()
+      ..moveTo(0.0, decoration.borderRadius)
+      ..quadraticBezierTo(0, 0, decoration.borderRadius, 0)
+      ..lineTo(size.width - decoration.borderRadius, 0.0)
+      ..quadraticBezierTo(size.width, 0, size.width, decoration.borderRadius)
+      ..lineTo(size.width, clipHeight - decoration.clipRadius)
+      ..arcToPoint(
+        Offset(size.width, clipHeight + decoration.clipRadius),
+        radius: Radius.circular(decoration.clipRadius),
+        clockwise: false,
+      )
+      ..lineTo(size.width, size.height - decoration.borderRadius)
+      ..quadraticBezierTo(size.width, size.height,
+          size.width - decoration.borderRadius, size.height)
+      ..lineTo(decoration.borderRadius, size.height)
+      ..quadraticBezierTo(
+          0, size.height, 0, size.height - decoration.borderRadius)
+      ..lineTo(0, clipHeight + decoration.clipRadius)
+      ..arcToPoint(
+        Offset(0, clipHeight - decoration.clipRadius),
+        radius: Radius.circular(decoration.clipRadius),
+        clockwise: false,
+      )
+      ..close();
 
     canvas.drawShadow(path, decoration.shadowColor, decoration.elevation, true);
     canvas.drawPath(path, paint);
-
-    // Inorder to hide the shadow on top of bottom view
-    var hackPaint = Paint()
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 10
-      ..color = decoration.bottomColor;
-    canvas.drawLine(
-      Offset(decoration.clipRadius, 0),
-      Offset(size.width - decoration.clipRadius, 0),
-      hackPaint,
-    );
-    // -----------------------
   }
 
   @override
@@ -174,8 +198,9 @@ class _BottomPainter extends CustomPainter {
 
 class _DividerPainter extends CustomPainter {
   final TicketDivider divider;
+  final double clipRatio;
 
-  _DividerPainter(this.divider);
+  _DividerPainter(this.divider, this.clipRatio);
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -191,11 +216,11 @@ class _DividerPainter extends CustomPainter {
 
     // Start to draw from left size.
     // Of course, you can change it to match your requirement.
-    double startX = 20;
-    double y = 0;
+    double startX = 0;
+    double y = size.height * clipRatio;
 
     // Repeat drawing until we reach the right edge.
-    while (startX < size.width - 20) {
+    while (startX < size.width) {
       // Draw a small line.
       canvas.drawLine(Offset(startX, y), Offset(startX + dashWidth, y), paint);
 
@@ -211,16 +236,12 @@ class _DividerPainter extends CustomPainter {
 }
 
 class TicketDecoration {
-  final Color topColor;
-  final Color bottomColor;
   final double borderRadius;
   final double clipRadius;
   final Color shadowColor;
   final double elevation;
 
   TicketDecoration({
-    this.topColor = Colors.white,
-    this.bottomColor = Colors.white,
     this.borderRadius = 10.0,
     this.clipRadius = 10.0,
     this.shadowColor = Colors.black,
@@ -238,6 +259,6 @@ class TicketDivider {
     this.dashWidth = 10.0,
     this.dashSpace = 5.0,
     this.dashHeight = 1.0,
-    this.color = Colors.grey,
+    this.color = Colors.black,
   });
 }
